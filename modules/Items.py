b9bcfe0a76971625c657ae2b637441bd4a6696b2
Korey <thekoreus.z@gmail.com>
2015-10-30 15:00:53 +0000

    Basic Weapons
    Added stone and knife weapons, py files from modules folder should be able to be imported now.

--- modules/Items.py
+++ modules/Items.py
@@ -1,3 +1,24 @@
 class Item():
-    self.name = name
-    self.description = description
+    def __init__(self, name, description):
+        
+        self.name = name
+        self.description = description
+
+class Weapon(Item):
+    def __init__(self, name, description, damage):
+    self.damage = damage
+    super().__init__(name, description, value)
+
+"""Basic Weapons in the section"""
+
+class Stone(Weapon):
+    def __init__(self):
+        super().__init__(name = "Cobble",
+                        description = "A fist-sized stone, with which to beat in enemy skulls.",
+                        damage = 5)
+
+class Knife(Weapon):
+    def __init__(self):
+        super().__init__(name = "Knife",
+                        description = "A flimsy Knife, intended for slicing meat. Small chance to inflic Bleeding",
+                        damage = 10)
