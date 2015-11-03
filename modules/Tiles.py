00ff515c1333fdc49e5ef3c3b73f982f952a30d6
Korey <thekoreus.z@gmail.com>
2015-11-03 17:34:55 +0000

    City tiles complete
    Completed all city tiles for now. Will add more when other systems are in place

--- modules/Tiles.py
+++ modules/Tiles.py
@@ -24,3 +24,47 @@ class StartRoom(MapTile):
         #This room has no actions
         pass
 
+class CityTile(MapTile):
+    def intro_text(self):
+        raise NotImplementedError()
+    def modify_player(self, player):
+        raise NotImplementedError()
+
+class Market(CityTile):
+    def intro_text(self):
+        return """
+        You walk into the marketplace of the city, it is filed with shops buying and selling items,
+        including weapons, armour, potions, and LOTS of useless junk.
+        
+        To Purchase any of these items, type Buy [weapons/armour/potions].
+        """
+    def modify_player(self, player):
+        #To do: Code for purchasing from a random selection of items that changes every 100 turns
+        pass
+
+class CityStreet(CityTile):
+    def intro_text(self):
+        return """
+        You walk down the city road. People are roaming about, but there is nothing to do here.
+        """
+    def modify_player(self, player):
+        pass
+
+class CityHome(CityTile):
+    def intro_text(self):
+        return """
+        You walk down the city road. There is a house for sale here. Type 'Buy house' to buy it.
+        """
+    def modify_player(self, player):
+        #once again, need to impliment a purchase system
+        pass
+
+class CityEdge(CityTile):
+    def intro_text(self):
+        return """
+        You've arrived at the edge of the city.
+        """
+    def modify_player(self, player):
+        pass
+
+#class ForestPath(MapTile):
