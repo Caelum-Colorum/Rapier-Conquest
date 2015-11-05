fc1ffe383d23a8ef8dc89dfadd469478e7f9daad
Korey <thekoreus.z@gmail.com>
2015-11-05 17:21:31 +0000

    letter case
    Case sensitivity is a bitch, ain't it?

--- modules/tiles.py
+++ modules/tiles.py
@@ -6,31 +6,57 @@ class MapTile():
         self.x = x
         self.y = y
         
+    def Type(self, tile_type):
+        self.tile_type = tile_type
+        
     def intro_text(self):
         raise NotImplementedError()
  
     def modify_player(self, player):
         raise NotImplementedError()
+
+
+#-----------------------------------------------------------------------------------------------------------
+#Tiles in this section are parent classes of game tiles, containing concepts to be used in many tiles
+
+
+class Purchase(MapTile):
+    def player_buy(self, items):
+        self.items = items
+        #This will contain code for purchases when I work out how to do that
+    
+    def intro_text(self):
+        raise NotImplementedError()
+    
+    def modify_player(self, player):
+        raise NotImplementedError()
+    
+class CityTile(MapTile):
+    def Type(self):
+        super().Type(tile_type = 'City')
         
+    def intro_text(self):
+        raise NotImplementedError()
+        
+    def modify_player(self, player):
+        raise NotImplementedError()
+
+
+#----------------------------------------------------------------------------
+#Tiles from here on may appear in game
+
+
 class StartRoom(MapTile):
     def intro_text(self):
         return """
-        We forgot to write some lore here, so pretend there's some bullshit backstory.
-        Here is a description of your surroundings: Trees. Now go adventure or something, I don't know...
-        Just don't stay here because thats boring.
+        
         """
     
     def modify_player(self, player):
         #This room has no actions
         pass
 
-class CityTile(MapTile):
-    def intro_text(self):
-        raise NotImplementedError()
-    def modify_player(self, player):
-        raise NotImplementedError()
-
-class Market(CityTile):
+class Market(CityTile, Purchase):
     def intro_text(self):
         return """
         You walk into the marketplace of the city, it is filed with shops buying and selling items,
@@ -38,11 +64,15 @@ class Market(CityTile):
         
         To Purchase any of these items, type Buy [weapons/armour/potions].
         """
+    
     def modify_player(self, player):
-        #To do: Code for purchasing from a random selection of items that changes every 100 turns
         pass
+    
+    super.player_buy(items = ['Stone', 'Knife'])
+                #To do: Code for purchasing from a random selection of items that changes every 100 turns.
 
 class CityStreet(CityTile):
+
     def intro_text(self):
         return """
         You walk down the city road. People are roaming about, but there is nothing to do here.
@@ -50,15 +80,18 @@ class CityStreet(CityTile):
     def modify_player(self, player):
         pass
 
-class CityHome(CityTile):
+class CityHome(CityTile,):
     def intro_text(self):
         return """
         You walk down the city road. There is a house for sale here. Type 'Buy house' to buy it.
         """
     def modify_player(self, player):
-        #once again, need to impliment a purchase system
+        #Need a home system, where after buying houses, the player repawns at the nearest. 
+        #If the player owns no house, they'll respawn at an inn.
         pass
 
+class CityInn(CityTile):
+    
 class CityEdge(CityTile):
     def intro_text(self):
         return """
@@ -67,4 +100,4 @@ class CityEdge(CityTile):
     def modify_player(self, player):
         pass
 
-#class ForestPath(MapTile):
+class ForestPath(MapTile):
